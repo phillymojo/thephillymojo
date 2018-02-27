d9717cc462ab5c223546f9e11e773137c173ed49
Morgan Bonar <mbonar@gmail.com>
2018-02-26 20:21:53 -0800

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Layout } from './Layout';
-// import { Homepage } from './Homepage';
-// import { Morgan } from './Morgan';
-// import { Renata } from './Renata';
 
 import { routes } from '../routes';
 
